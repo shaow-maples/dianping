549b22a0c7e5695f89e7feac83c7d90c60d0a081
王海龙 <ismaples@163.com>
2023-05-16 17:22:58 +0800

    fix：店铺信息缓存穿透问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -43,10 +43,17 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+        //判断命中的是否是空值
+        if (shopJson != null) {
+            //返回错误信息
+            return Result.fail("店铺信息不存在！！");
+        }
         //4. 不存在，根据id查询数据库
         Shop shop = getById(id);
         if (Objects.isNull(shop)) {
-            //5. 数据库中不存在，直接返回错误
+            //5. 数据库中不存在，返回错误
+            //将空值写入redis
+            stringRedisTemplate.opsForValue().set(key, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
             return Result.fail("店铺不存在！");
         }
         //6. 数据库中存在，写入redis
